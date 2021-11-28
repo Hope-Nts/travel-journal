56c27bcba85457a52b409ce5620852188057d20b
tshenolo17 <ntshononohope@gmail.com>
2021-11-27 18:32:13 -0800

    added the home fragment layout

--- app/src/main/java/com/example/traveljournal/MainActivity.java
+++ app/src/main/java/com/example/traveljournal/MainActivity.java
@@ -1,19 +1,77 @@
 package com.example.traveljournal;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.traveljournal.Fragments.HomeFragment;
+import com.example.traveljournal.Fragments.SettingsFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+     private BottomNavigationView bottomNavigationView;
+     private Fragment selectorFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.nav_home:
+                        selectorFragment = new HomeFragment();
+                        break;
+                    case R.id.nav_settings:
+                        selectorFragment = new SettingsFragment();
+                        break;
+                    case R.id.nav_add:
+                        selectorFragment = null;
+                        startActivity(new Intent( MainActivity.this, JournalEntryActivity.class));
+                        break;
+                }
+                if(selectorFragment != null){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectorFragment).commit();
+                }
+                return true;
+            }
+        });
+
+//        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+//            @Override
+//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//                switch (item.getItemId()){
+//                    case R.id.nav_home:
+//                        selectorFragment = new HomeFragment();
+//                        break;
+//                    case R.id.nav_settings:
+//                        selectorFragment = new SettingsFragment();
+//                        break;
+//                    case R.id.nav_add:
+//                        selectorFragment = null;
+//                        startActivity(new Intent( MainActivity.this, JournalEntryActivity.class));
+//                        break;
+//                }
+//                if(selectorFragment != null){
+//                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectorFragment).commit();
+//                }
+//                return true;
+//            }
+//        });
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
 
     }
 }
